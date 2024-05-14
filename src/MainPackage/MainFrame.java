18a4dccdb46af01666ef4d97ebf1d290171296c1
daniil54sharov <daniilsharov54@gmail.com>
2024-05-14 22:56:56 +0300

    iotodo_class_task_3/4

--- src/MainPackage/MainFrame.java
+++ src/MainPackage/MainFrame.java
@@ -3,28 +3,40 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
-import java.awt.*;
+import java.awt.GridLayout;
+import java.awt.Dimension;
+import java.awt.Component;
+import java.io.File;
+import java.util.ArrayList;
 
 public final class MainFrame extends JFrame{
-
     private final JPanel mainPanel = new JPanel(new GridLayout(0, 1));
     private final JScrollPane mainScrollPane = new JScrollPane(mainPanel);
+    private final ArrayList<ToDo> toDoArrayList = new ArrayList<>();
     MainFrame(String appName) {
         super(appName);
         this.add(mainScrollPane);
     }
 
     public static void main(String[] args) {
-        MainFrame mf = new MainFrame("ToDo list");
-        mf.setVisible(true);
+        MainFrame mainFrame = new MainFrame("ToDo list");
+
+//        for(int i = 0; i < 10; i++) {
+//            IOToDo.writeTask(new ToDo("ToDo" + (i + 1)));
+//        }
+
+        String[] fileNameList = IOToDo.getList("Data/");
+
         for(int i = 0; i < 10; i++) {
             JPanel panel = new JPanel();
             panel.setPreferredSize(new Dimension(600, 200));
-            panel.add(new JLabel("String " + i));
-            mf.addToMainFrame(panel);
+            panel.add(new JLabel(IOToDo.readTask(fileNameList[i]).getTopic()));
+            mainFrame.addToMainFrame(panel);
         }
-        mf.initFrame();
-        mf.initMainScrollPane();
+
+        mainFrame.setVisible(true);
+        mainFrame.initFrame();
+        mainFrame.initMainScrollPane();
     }
 
     private void initFrame() {
